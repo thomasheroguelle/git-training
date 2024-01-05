770e9db6955a8888f68276016836674c6b36a7a1
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-01-05 19:01:35 +0100

    create note for squash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PushComponent } from './components/push/push.component';
 import { HomeComponent } from './components/home/home.component';
 import { AmendComponent } from './components/amend/amend.component';
 import { GraphComponent } from './components/graph/graph.component';
+import { CherrypickComponent } from './components/cherrypick/cherrypick.component';
+import { SquashComponent } from './components/squash/squash.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'push', component: PushComponent },
   { path: 'amend', component: AmendComponent },
   { path: 'graph', component: GraphComponent },
+  { path: 'cherrypick', component: CherrypickComponent },
+  { path: 'squash', component: SquashComponent },
 ];
 
 
